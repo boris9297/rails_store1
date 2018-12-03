6279d0cdc91c59836669a53758a3a4416c9b6204
boris9297 <boris9297@gmail.com>
2018-12-02 16:36:33 -0800

    part1 finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'shopper#index', as: 'root' #把主页指向shopper/index
+  get 'shopper', to: 'shopper#index'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
